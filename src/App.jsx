a656af1b34727161c06acd3d7699e115295799a5
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-01-03 18:42:23 +0900

    라우트,초기화면 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
