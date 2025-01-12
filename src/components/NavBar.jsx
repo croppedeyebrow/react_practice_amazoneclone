41f381c787059aa405d4c9553515351cffa715f8
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-01-12 18:15:21 +0900

    상단바 작업중

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,13 @@ const NavBar = () => {
     <header className="min-w-[1000px]">
       <div className="flex bg-amazonclone text-white h-[60px]">
         {/* 왼 */}
-        <div className="flex">
-          <img src={"/images/amazon.png"} alt="logo_img" />
-          <div>
+        <div className="flex items-center">
+          <img
+            className="h-[36px] w-[100px] m-2"
+            src={"/images/amazon.png"}
+            alt="logo_img"
+          />
+          <div className="pr-4 pl-4">
             <div className="text-xs xl:text-sm">Deliver to</div>
             <div className="text-sm xl:text-base font-bold">
               Republic of Korea
@@ -17,7 +21,19 @@ const NavBar = () => {
         {/* 중앙 */}
         <div className="flex">Middle</div>
         {/* 우 */}
-        <div className="flex">Right</div>
+        <div className="flex items-center">
+          <div className="pr-4 pl-4">
+            <div className="text-xs xl:text-sm">Hello, Sign in</div>
+            <div className="text-sm xl:text-base font-bold">
+              Accounts & Lists
+            </div>
+          </div>
+          <div className="pr-4 pl-4">
+            <div className="text-xs xl:text-sm">Return</div>
+            <div className="text-sm xl:text-base font-bold">& Orders</div>
+            <div className="flex pr-3 pl-3"></div>
+          </div>
+        </div>
       </div>
     </header>
   );
